61faa71f0865a2dcbbff5a77a7dbebc56abb3c0d
Kasbe14 <kaustubh14kasbe2000@gmail.com>
2026-06-24 14:18:22 +0000

    chore: move datat sturtures to new package

---
+++ model/models.go
@@ -0,0 +1,26 @@
+package model
+
+import  (
+    "time"
+)
+
+type Users struct {
+	UserID         int64  //
+	Name           string // username
+	PassHash       string //salted hash of the password
+    EncryptSalt    []byte //salt for derived key for masterkey
+	WrappedKeyPass []byte //masterkey locked by masterpassword [encrypted ciphertext AES-GCM nonce 12bytes appended at front]
+	WrappedkeyRec  []byte //master key lockey by string given to user  nonce 12bytes appended at front
+}
+
+type Profile struct {
+	ProfileID      int64
+	UserID         int64  //foreign key from the user
+	ProfileHash    []byte //hmac-Sha256 of profile name blind indexing
+	EncProfileName []byte // profile name for a account eg gmail, instagram etc[encrypted ciphertext AES-GCM nonce 12bytes appended at front]
+	EncProfilePass []byte // password for the profile [encypted ciphertext AES-GCM]
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	UnlockAT       int64 //locked password will be unlocked after unix epoch integer
+	Locked         bool  //indicates locked password
+}

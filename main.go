f56bb7a6f815ddb8978eca4fddc2cd35db2beb5f
Kasbe14 <kaustubh14kasbe2000@gmail.com>
2026-06-25 11:38:50 +0000

    chore: driver code to util functions

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	// "github.com/Kasbe14/PassMan/core"
+	"fmt"
 	"log"
-
-	"github.com/Kasbe14/PassMan/database"
+    "github.com/Kasbe14/PassMan/database"
+	"github.com/Kasbe14/PassMan/core"
 )
 
 func main() {
@@ -20,44 +20,48 @@ func main() {
 	}
 
 	fmt.Println("successfully connected to the SQLite database")*/
-	// var userPass string
-	// // fakePass := "sunStone14"
-	// fmt.Print("Enter password: ")
-	// _, err := fmt.Scanf("%s\n", &userPass)
-	// if err != nil {
-	// 	fmt.Printf("error:%s\n", err)
-	// 	return
-	// }
+	var userPass string
+	// fakePass := "sunStone14"
+	fmt.Print("Enter password: ")
+	_, err := fmt.Scanf("%s\n", &userPass)
+	if err != nil {
+		fmt.Printf("error:%s\n", err)
+		return
+	}
 
-	// hash, err := core.CreateSaltedHash(userPass)
-	// if err != nil {
-	// 	log.Fatalf("failed to create user: %v", err)
-	// }
-	// fmt.Printf("user created with pass hash: %s\n", hash)
-	// var enterePassword string
-	// fmt.Print("Enter password to authenticate:")
-	// _, err = fmt.Scanf("%s", &enterePassword)
-	// if err != nil {
-	// 	fmt.Printf("error:%s", err)
-	// 	return
-	// }
-	// Valid, err := core.AuthenticateUser(enterePassword, hash)
-	// if err != nil {
-	// 	log.Fatalf("user authentication failed: %v", err)
-	// }
-	// if !Valid && err == nil {
-	// 	log.Fatalf("user authentication failed:")
-	// }
-	// if Valid {
-	// 	fmt.Println("user authentication success")
-	// }
+	hash, err := core.CreateSaltedHash(userPass)
+	if err != nil {
+		log.Fatalf("failed to create user: %v", err)
+	}
+	fmt.Printf("user created with pass hash: %s\n", hash)
+	var enterePassword string
+	fmt.Print("Enter password to authenticate:")
+	_, err = fmt.Scanf("%s", &enterePassword)
+	if err != nil {
+		fmt.Printf("error:%s", err)
+		return
+	}
+	Valid, err := core.AuthenticateUser(enterePassword, hash)
+	if err != nil {
+		log.Fatalf("user authentication failed: %v", err)
+	}
+	if !Valid && err == nil {
+		log.Fatalf("user authentication failed:")
+	}
+	if Valid {
+		fmt.Println("user authentication success")
+	}
 	db, err := database.NewDatabase()
 	if err != nil {
 		log.Fatalf("failed to open database: %v", err)
+	} else {
+		fmt.Println("db connected successfully")
 	}
 	defer database.CloseDB(db)
 	if err := database.InitializeSchema(db); err != nil {
 		log.Fatalf("failed to initialize database: %v", err)
+	} else {
+		fmt.Println("db schema initialize success")
 	}
 
 }

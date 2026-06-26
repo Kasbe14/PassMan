a377d96fcebf57e8d0c9ab0e6e6d8d1a1f935298
root <root@SUNSTONE.>
2026-06-26 14:49:21 +0000

    implement: DTO for profiles

--- model/models.go
+++ model/models.go
@@ -1,8 +1,7 @@
 package model
 
-import  (
-    "time"
-)
+// import "time"
+
 
 type Users struct {
 	UserID         int64  //
@@ -16,11 +15,21 @@ type Users struct {
 type Profile struct {
 	ProfileID      int64
 	UserID         int64  //foreign key from the user
-	ProfileHash    []byte //hmac-Sha256 of profile name blind indexing
+	ProfileHash    string //hmac-Sha256 of profile name blind indexing
 	EncProfileName []byte // profile name for a account eg gmail, instagram etc[encrypted ciphertext AES-GCM nonce 12bytes appended at front]
 	EncProfilePass []byte // password for the profile [encypted ciphertext AES-GCM]
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	CreatedAt      int64  //unix time
+	UpdatedAt      int64 //unit time
 	UnlockAT       int64 //locked password will be unlocked after unix epoch integer
 	Locked         bool  //indicates locked password
+}
+
+//dto
+type DecryptedProfile struct {
+    Name string
+    Password string
+    CreatedAt  int64
+    UpdatedAt int64
+    Locked     bool
+    UnlockAt   int64
 }

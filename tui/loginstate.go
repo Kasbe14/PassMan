613489d2c90e06d83d6b5b07c614189630b42be6
Kasbe14 <kaustubh14kasbe2000@gmail.com>
2026-07-05 15:48:09 +0000

    add : loginstate

--- tui/loginstate.go
+++ tui/loginstate.go
@@ -1,1 +1,31 @@
 package tui
+
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+) 
+
+var (
+
+)
+
+type LoginModel struct {
+    title string
+    inputs  []textinput.Model
+    quitLogin bool
+}
+
+
+func (lm LoginModel) Init() tea.Cmd {
+
+    return textinput.Blink
+
+}
+func (lm LoginModel) Update(msg tea.Msg) (tea.Model,tea.Cmd) {
+    return lm,nil
+}
+func (lm LoginModel) View() string {
+
+    return "hello form Login model"
+
+}
